e945ba4dcb5502bf51a626ee6dda31e9e10ead43
Hung <tonic1307@gmail.com>
2022-05-24 14:27:34 +0700

    fix inconsistent

--- model/sequence/bilstm.py
+++ model/sequence/bilstm.py
@@ -5,7 +5,7 @@ from torch import nn
 class BRNN(nn.Module):
     def __init__(self, input_size, vocab_size):
         super(BRNN, self).__init__()
-        self.bilstm1 = nn.LSTM(input_size, hidden_size=512, dropout=0.2, bidirectional=True,
+        self.bilstm1 = nn.LSTM(input_size, hidden_size=256, dropout=0.2, bidirectional=True,
                                batch_first=True, num_layers=2)
         # self.bilstm2 = nn.LSTM(512, 512, dropout=0.2, bidirectional=True,
         #                        batch_first=True)
@@ -13,8 +13,8 @@ class BRNN(nn.Module):
                                 nn.Softmax(vocab_size + 1)
                                 )
     def _init_hidden(self, x):
-        return (torch.zeros((4, x.shape[0], 512)), 
-                torch.zeros((4, x.shape[0], 512)))
+        return (torch.zeros((4, x.shape[0], 256), device=x.device),
+                torch.zeros((4, x.shape[0], 256), device=x.device))
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """
         :param x: shape (N,L,H): Batch size, Length seq, Channels
